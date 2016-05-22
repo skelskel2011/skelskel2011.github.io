0e29605bb07de3cf25412caaa877483ce0377e56
unknown <skelskel2011@yandex.ru>
2016-05-22 23:40:09 +0300

    fix bugs

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 $(function() {
     $('.main-header__menu-close').on('click', function() {
         $('.main-header__menu-wrapper').removeClass('active');
-    })
+    });
     
     $('.main-header__ham').on('click', function() {
         $('.main-header__menu-wrapper').addClass('active');
+    });
+    
+    $('.main-content-item-menu__item').on('click', function(e) {
+        var index = $(this).index();
+        
+        $('.main-content-item-menu__item').removeClass('active');
+        $('.main-content-item-menu__item').eq(index).addClass('active');
+        $('.main-content-item__content').removeClass('active');
+        $('.main-content-item__content').eq(index).addClass('active');
     });
 });
